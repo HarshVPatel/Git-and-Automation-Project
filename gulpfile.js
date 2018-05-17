c4f4eb1d41fd3ef04bb0479f1c6751e075a36bcf
SD16 <SD16@mitt.ca>
2018-05-17 15:41:38 -0500

    autoprfixing the css

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp')
+const autoprefixer = require('gulp-autoprefixer')
+const babel = require('gulp-babel')
+const imagemin = require('gulp-imagemin')
+const concat = require('gulp-concat')
+const uglify = require('gulp-uglify')
+
+
+gulp.task('css', () =>
+    gulp.src('src/*.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist'))
+);
