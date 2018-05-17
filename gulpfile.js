7cae1ed9de25e20a0bd28c70d08053b8a6b863c4
SD16 <SD16@mitt.ca>
2018-05-17 15:48:35 -0500

    clean css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp')
 const autoprefixer = require('gulp-autoprefixer')
+const cleanCSS = require('gulp-clean-css');
 const babel = require('gulp-babel')
 const imagemin = require('gulp-imagemin')
 const concat = require('gulp-concat')
@@ -7,10 +8,12 @@ const uglify = require('gulp-uglify')
 
 
 gulp.task('css', () =>
-    gulp.src('src/*.css')
+    gulp.src('src/css/*.css')
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest('dist'))
+
 );
